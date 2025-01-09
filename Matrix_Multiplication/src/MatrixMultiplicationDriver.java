65736c770bb25fc8e122c3a6e32b3af7b52c9a7d
kishore <nandhakishorejajula@gmail.com>
2025-01-09 10:30:29 +0530

    matrix multiplication new

--- Matrix_Multiplication/src/MatrixMultiplicationDriver.java
+++ Matrix_Multiplication/src/MatrixMultiplicationDriver.java
@@ -1,43 +1,54 @@
-//MatrixMultiplyDriver.java:
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.Path; 
+import org.apache.hadoop.conf.*; 
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; 
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat; 
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat; 
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
-public class MatrixMultiplicationDriver extends Configured implements Tool {
 
-    public int run(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: MatrixMultiplyDriver <input path> <output path>");
-            System.exit(-1);
-        }
+public class MatrixMultiplicationDriver 
 
-        Job job = Job.getInstance(getConf(), "Matrix Multiplication");
-        job.setJarByClass(MatrixMultiplicationDriver.class);
-        
-        job.setMapperClass(MatrixMultiplicationMapper.class);
-        job.setReducerClass(MatrixMultiplicationReducer.class);
-        
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-        
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+{
 
-        TextInputFormat.addInputPath(job, new Path(args[0]));
-        TextOutputFormat.setOutputPath(job, new Path(args[1]));
+	public static void main(String[] args) throws Exception 
 
-        return job.waitForCompletion(true) ? 0 : 1;
-    }
+	{
 
-    public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new MatrixMultiplicationDriver(), args);
-        System.exit(exitCode);
-    }
+		Configuration conf = new Configuration();
+
+		// M is an m-by-n matrix; N is an n-by-p matrix. 
+
+		conf.set("m", "2");
+
+		conf.set("n", "2");
+
+		conf.set("p", "2");
+
+		Job job = Job.getInstance(conf, "MatrixMultiplication"); 
+
+		job.setJarByClass(MatrixMultiplicationDriver.class); 
+
+		job.setOutputKeyClass(Text.class); 
+
+		job.setOutputValueClass(Text.class);
+
+		job.setMapperClass(MatrixMultiplicationMapper.class); 
+
+		job.setReducerClass(MatrixMultiplicationReducer.class);
+
+		job.setInputFormatClass(TextInputFormat.class); 
+
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+
+		FileOutputFormat.setOutputPath(job, new Path(args[1])); 
+		boolean success = job.waitForCompletion(true);
+
+		System.exit(success?0:1);
+
+}
+
 }
--- Matrix_Multiplication/src/MatrixMultiplicationMapper.java
+++ Matrix_Multiplication/src/MatrixMultiplicationMapper.java
@@ -1,23 +1,67 @@
-//MatrixMultiplyMapper.java
+import java.io.IOException; 
+
+import org.apache.hadoop.conf.*; 
+
 import org.apache.hadoop.io.*;
+
 import org.apache.hadoop.mapreduce.*;
 
-import java.io.IOException;
+public class MatrixMultiplicationMapper extends Mapper<LongWritable, Text, Text, Text> 
 
-public class MatrixMultiplicationMapper extends Mapper<LongWritable, Text, Text, Text> {
+{
 
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] tokens = value.toString().split(",");
-        int i = Integer.parseInt(tokens[0].trim());
-        int j = Integer.parseInt(tokens[1].trim());
-        int val = Integer.parseInt(tokens[2].trim());
+public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
 
-        if (tokens.length == 3) {
-            // Matrix A (i, j, value)
-            context.write(new Text(i + ","), new Text("A," + j + "," + val));
-        } else {
-            // Matrix B (j, k, value)
-            context.write(new Text(j + ","), new Text("B," + i + "," + val));
-        }
-    }
+{
+
+Configuration conf = context.getConfiguration(); 
+
+int m = Integer.parseInt(conf.get("m"));
+
+int p = Integer.parseInt(conf.get("p")); 
+
+String line = value.toString();
+
+String[] indicesAndValue = line.split(","); 
+
+Text outputKey = new Text();
+
+Text outputValue = new Text();
+
+if (indicesAndValue[0].equals("M")) 
+
+{ 
+
+	for (int k = 0; k < p; k++)
+
+{
+
+outputKey.set(indicesAndValue[1] + "," + k);
+
+outputValue.set("M," + indicesAndValue[2] + "," + indicesAndValue[3]); 
+
+context.write(outputKey, outputValue);
+
+}
+
+} 
+
+else 
+
+{
+
+for (int i = 0; i < m; i++) 
+
+{
+
+outputKey.set(i + "," + indicesAndValue[2]);
+
+outputValue.set("N," + indicesAndValue[1] + "," + indicesAndValue[3]); context.write(outputKey, outputValue);
+
+}
+
+}
+
+}
+
 }
